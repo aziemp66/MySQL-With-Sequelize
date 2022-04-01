8bb76756e5733d04bf6bd67dfd8566509fec2745
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-01 11:45:13 +0700

    Updating API

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -8,6 +8,20 @@ router.get("/all", (req, res) => {
 	});
 });
 
+router.get("/find/:id", (req, res) => {
+	db.Todo.findByPk(req.params.id, {
+		attributes: {
+			exclude: ["id"],
+		},
+	})
+		.then((todo) => {
+			res.json(todo);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
 router.post("/add", (req, res) => {
 	db.Todo.create({
 		id: uuid(),
@@ -42,25 +56,6 @@ router.patch("/update/:id", (req, res) => {
 	db.Todo.update(
 		{
 			title: req.body.title,
-			completed: req.body.completed,
-		},
-		{
-			where: {
-				id: req.params.id,
-			},
-		}
-	)
-		.then((todo) => {
-			res.json(todo);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
-});
-
-router.patch("/progress/:id", (req, res) => {
-	db.Todo.update(
-		{
 			completed: req.body.completed,
 		},
 		{
