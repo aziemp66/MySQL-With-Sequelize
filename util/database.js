3f44617708d6ec06f44381b1f366219afd81e118
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-01 09:33:59 +0700

    Reseting Project

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("ks-store", "root", "9871709924", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
